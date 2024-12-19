2464650e3c9287eeda3540d64ba24b67efe075a7
Saumya Verma <vsaumyav20@gmail.com>
2024-12-19 21:52:12 +0530

    Added delete and toggle functionality

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -1,7 +1,51 @@
 package com.app.todoapp.controller;
 
+import com.app.todoapp.models.Task;
+import com.app.todoapp.services.TodoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
+    private final TodoService todoService;
+
+    public TaskController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = todoService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+        //as we are using thymleaf it is going to look for task.hmtl in resources
+    }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title) {
+        todoService.createTask(title);
+        return "redirect:/";
+        //as we are using thymleaf it is going to look for task.hmtl in resources
+    }
+
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        todoService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        todoService.toggleTask(id);
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/app/todoapp/models/Task.java
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -14,6 +14,30 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     private boolean completed;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
